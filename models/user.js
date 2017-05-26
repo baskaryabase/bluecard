5d9a76244196c3781ff0ced77609caef18ae4ccb
smilysyndrella <baymax1298@gmail.com>
2017-05-26 12:56:08 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,8 +4,7 @@ var passportLocalMongoose = require("passport-local-mongoose");
 var userSchema =  new mongoose.Schema({
     username: String,
     password: String
-  
-});
+  });
 userSchema.plugin(passportLocalMongoose);
 module.exports =mongoose.model("user",userSchema);
 
